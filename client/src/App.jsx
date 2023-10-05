f03c4c8751ff44f21a6ed98d61c35b7850c02584
daniel-idowu-01 <danielidowu414@gmail.com>
2023-10-05 01:03:41 +0100

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home, About, SignIn, SignOut, Profile } from './pages'
+
 function App() {
 
   console.log(2)
-  
+
   return (
-    <>
-      <p className='text-red-500'>MEE</p>
-        
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import About from "./About";
+import SignIn from "./SignIn";
+import SignOut from "./SignOut";
+import Profile from "./Profile";
+
+export {
+    Home, About, SignIn, SignOut, Profile
+}
